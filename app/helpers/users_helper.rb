187331a9112242dbdf3982d531ea1d6424a0b939
Pietro Di Bello <orione-team+dip@xpeppers.com>
2012-06-04 16:36:07 +0200

    use helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def format_user(user)
+    "<tr>
+    <td>#{user.first_name}</td>
+    <td>#{user.last_name}</td>
+    <td>#{user.telephone_number}</td>
+    <td>"+(link_to 'Edit', edit_user_path(user))+"|"+(link_to 'Destroy', user, confirm: 'Are you sure?', method: :delete)+"</td>
+    </tr>"
+  end
 end
